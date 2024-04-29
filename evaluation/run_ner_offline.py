62eca2d58f055f7f628bfc306364f019c348a7c3
Mathieu Lai-King <laiking@lisn.fr>
2024-04-29 19:43:09 +0200

    added cache disabling in run ner
    because errors with evaluate metric loading

--- evaluation/run_ner_offline.py
+++ evaluation/run_ner_offline.py
@@ -28,7 +28,7 @@ from typing import Optional
 import datasets
 import evaluate
 import numpy as np
-from datasets import ClassLabel, load_dataset
+from datasets import ClassLabel, load_dataset, disable_caching
 
 import transformers
 from transformers import (
@@ -181,7 +181,7 @@ def main():
     # See all possible arguments in src/transformers/training_args.py
     # or by passing the --help flag to this script.
     # We now keep distinct sets of args, for a cleaner separation of concerns.
-
+    
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
@@ -189,7 +189,10 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-
+    
+    # We disable the cache for preventing errors with evaluate
+    disable_caching()
+    
     # Setup WANDB variables
     os.environ["WANDB_MODE"] = "offline"
     os.environ["WANDB_PROJECT"] = "blurb-ner-finetune"
