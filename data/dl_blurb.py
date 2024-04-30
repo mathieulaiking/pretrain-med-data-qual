d39e5e667ebcb64aafa0761a8ebf5376e5ef2cdf
Mathieu Lai-King <laiking@lisn.fr>
2024-04-30 19:29:28 +0200

    added fine tuning scripts , updated biosses preprocessing

--- data/dl_blurb.py
+++ data/dl_blurb.py
@@ -1,6 +1,6 @@
 import argparse
 import datasets
-
+import numpy as np
 
 _DATASETS_TASKS_CONFIGS = {
     "token_classification" : [
@@ -36,7 +36,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--cache_dir",
-        default=".blurb_cache",
+        default=None,
         type=str,
         help="Path to cache directory where datasets will be stored"
     )
@@ -58,6 +58,16 @@ def parse_args():
         raise ValueError(f"Task {args.task} not found in available tasks, available tasks are: {_DATASETS_TASKS_CONFIGS.keys()}")
     return args
 
+def _preprocess(ds:datasets.DatasetDict, num_proc:int):
+    if ds["train"].info.dataset_name == "biosses":
+        ds=ds.map(
+            lambda examples: examples | {"annotator_avg": np.mean([examples[k] for k in examples if "annotator" in k],axis=0)},
+            batched=True,
+            remove_columns=[f"annotator_{x}" for x in "abcde"],
+            num_proc=num_proc
+        )
+    return ds
+
 def main():
     args = parse_args()
     # download NER datasets
@@ -65,13 +75,22 @@ def main():
         if args.task is not None and task != args.task:
             continue
         for dataset,config in datasets_configs:
+            # Download dataset
             print("Downloading dataset", dataset, "with config", config)
-            datasets.load_dataset(
+            ds = datasets.load_dataset(
                 dataset,
                 name=config,
                 cache_dir=args.cache_dir,
                 num_proc=args.num_proc,
             )
+            # Preprocess dataset
+            preproc_ds = _preprocess(ds,args.num_proc)
+            # Save on disk
+            out_dir = dataset.split('/')[-1]
+            if config is not None:
+                out_dir += f"_{config}"
+            preproc_ds.save_to_disk(out_dir)
+            
 
 if __name__ == "__main__":
     main()
