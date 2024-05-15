c0156672ea3b99c3bcc23c989fbd6280c10d7b39
Mathieu Lai-King <laiking@lisn.fr>
2024-05-15 11:47:28 +0200

    micro average for f1

--- evaluation/run_relation_extraction.py
+++ evaluation/run_relation_extraction.py
@@ -376,7 +376,7 @@ def main():
     def compute_metrics(p: EvalPrediction):
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
         preds = np.argmax(preds, axis=1)
-        result = metric.compute(predictions=preds, references=p.label_ids)
+        result = metric.compute(predictions=preds, references=p.label_ids, average="micro")
         if len(result) > 1:
             result["combined_score"] = np.mean(list(result.values())).item()
         return result
